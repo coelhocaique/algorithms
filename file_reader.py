1d092cf69f01921dcb69c5289b5d6f886920a30c
coelhocaique <coelhocaiquesantos@hotmail.com>
2019-04-22 00:28:11 -0300

    strongly connected components

--- file_reader.py
+++ file_reader.py
@@ -6,6 +6,14 @@ class FilePath:
     INVERSIONS = '/inversions.txt'
     COMPARISONS = '/comparisons.txt'
     MIN_CUT = '/min_cut.txt'
+    SCC = '/scc.txt'
+
+
+def read_file(filename):
+    f = open(os.path.abspath('data') + filename, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
 
 
 def get_input_as_list(filename):
@@ -22,9 +30,7 @@ def get_input_as_list(filename):
 
 def get_dict_as_adj_list(filename):
     adj_list = dict()
-    file = open(os.path.abspath('data') + filename, 'r')
-    lines = file.readlines()
-    file.close()
+    lines = read_file(filename)
 
     lines = map(lambda s: re.sub('\s+', ' ', str(s.strip('\r\n'))).strip(), lines)
     lines = map(lambda s: s.split(' '), lines)
@@ -33,3 +39,29 @@ def get_dict_as_adj_list(filename):
         adj_list[int(line[0])] = list(map(lambda s: int(s), line[1:]))
 
     return adj_list
+
+
+def read_scc_input(filename):
+    g, g_inverted = {}, {}
+    nodes = set()
+
+    lines = map(lambda s: s.strip().split(), read_file(filename))
+
+    for line in lines:
+        src = int(line[0])
+        dst = int(line[1])
+
+        if src in g.keys():
+            g.get(src).append(dst)
+        else:
+            g[src] = [dst]
+
+        if dst in g_inverted.keys():
+            g_inverted.get(dst).append(src)
+        else:
+            g_inverted[dst] = [src]
+
+        nodes.add(src)
+        nodes.add(dst)
+
+    return g, g_inverted, len(nodes)
