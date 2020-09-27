638723e06d33adc026ca2198cf1baf6f59836f34
coelhocaique <coelhocaiquesantos@hotmail.com>
2020-09-27 04:22:24 -0300

    corrects cluster bit implementation

--- greedy/clustering_bit.py
+++ greedy/clustering_bit.py
@@ -6,67 +6,75 @@ from union_find import UnionFind
 
 
 def get_data():
-
     lines = list(map(lambda s: s.split(), read_input(FilePath.CLUSTERING_BITS)))
     num_nodes = int(lines[0][0])
-    g = dict()
+    nodes = dict()
 
     for i in range(1, len(lines)):
         node = ''.join(lines[i])
-        g[node] = i
+        nodes[node] = nodes.get(node, []) + [i]
 
-    return g, num_nodes
+    return nodes, num_nodes
+
+
+def bit_shift(bit):
+    return '0' if int(bit) else '1'
 
 
 def get_one_bit_candidates(node, g):
     candidates = []
     for i in range(len(node)):
-        change_to = '1' if node[i] == '0' else '0'
+        change_to = bit_shift(node[i])
         candidate = node[:i] + change_to + node[i+1:]
         candidate_index = g.get(candidate, None)
 
         if candidate_index is not None:
-            candidates.append(candidate_index)
+            candidates += candidate_index
 
     return candidates
 
 
 def get_two_bit_candidates(node, g):
     candidates = []
+
     for i in range(len(node)):
-        i_change_to = '1' if node[i] == '0' else '0'
-        i_changed = node[:i] + i_change_to + node[i+1:]
-        for j in range(len(node)):
-            j_change_to = '1' if node[i] == '0' else '0'
-            candidate = i_changed[:j] + j_change_to + i_changed[j+1:]
+        first_bit_change_to = bit_shift(node[i])
+        node_first_bit_changed = node[:i] + first_bit_change_to + node[i+1:]
+        for j in range(i + 1, len(node)):
+            second_bit_change_to = bit_shift(node[j])
+            candidate = node_first_bit_changed[:j] + second_bit_change_to + node_first_bit_changed[j+1:]
             candidate_index = g.get(candidate, None)
 
             if candidate_index is not None:
-                candidates.append(candidate_index)
+                candidates += candidate_index
 
     return candidates
 
 
-def clustering(g, num_nodes):
+def clustering(nodes, num_nodes):
     uf = UnionFind(num_nodes)
 
-    for node in g.keys():
-        u = g.get(node)
-        one_bit_candidates = get_one_bit_candidates(node, g)
-        two_bit_candidates = get_two_bit_candidates(node, g)
+    for node in nodes.keys():
+        us = nodes.get(node)
 
-        for v in one_bit_candidates:
-            uf.union(u, v)
+        one_bit_candidates = get_one_bit_candidates(node, nodes)
+        two_bit_candidates = get_two_bit_candidates(node, nodes)
+        candidates = one_bit_candidates + two_bit_candidates
 
-        for v in two_bit_candidates:
-            uf.union(u, v)
+        if len(us) > 1:
+            for i in range(len(us)):
+                for j in range(i + 1, len(us)):
+                    uf.union(us[i], us[j])
 
-    return len(set(uf.leaders())) - 1
+        for v in candidates:
+            uf.union(us[0], v)
+
+    return uf.size()
 
 
-g, num_nodes = get_data()
+nodes, num_nodes = get_data()
 
-max_distance = clustering(g, num_nodes)
+max_distance = clustering(nodes, num_nodes)
 
 print('Clustering bit max distance:', max_distance)  # 6118
 
